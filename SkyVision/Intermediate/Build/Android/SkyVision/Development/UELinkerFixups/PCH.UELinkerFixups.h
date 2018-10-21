1872c9af4f6f785be9bea718422dbc22a8b146b9
daniloBH <37309058+daniloBH@users.noreply.github.com>
2018-10-21 19:25:03 -0300

    Tentativa Inicial de Commit

---
+++ SkyVision/Intermediate/Build/Android/SkyVision/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for C:\Users\Pichau\Documents\Unreal Projects\SkyVision\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h
+#include "C:\Users\Pichau\Documents\Unreal Projects\SkyVision\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h"
---
+++ SkyVision/Intermediate/Source/SkyVision.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SkyVisionTarget : TargetRules
+{
+	public SkyVisionTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.Add("UE4Game");
+	}
+}
