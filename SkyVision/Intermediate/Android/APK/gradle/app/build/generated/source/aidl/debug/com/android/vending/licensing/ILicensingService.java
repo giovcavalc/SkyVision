a2848c966f230dc48d644032116e60a844b6d749
giovcavalc <giovcavalc@gmail.com>
2018-12-10 18:08:16 -0200

    Ajustes finais

--- SkyVision/Intermediate/Android/APK/gradle/app/build/generated/source/aidl/debug/com/android/vending/licensing/ILicensingService.java
+++ SkyVision/Intermediate/Android/APK/gradle/app/build/generated/source/aidl/debug/com/android/vending/licensing/ILicensingService.java
@@ -35,16 +35,17 @@ return this;
 }
 @Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
 {
+java.lang.String descriptor = DESCRIPTOR;
 switch (code)
 {
 case INTERFACE_TRANSACTION:
 {
-reply.writeString(DESCRIPTOR);
+reply.writeString(descriptor);
 return true;
 }
 case TRANSACTION_checkLicense:
 {
-data.enforceInterface(DESCRIPTOR);
+data.enforceInterface(descriptor);
 long _arg0;
 _arg0 = data.readLong();
 java.lang.String _arg1;
@@ -54,8 +55,11 @@ _arg2 = com.android.vending.licensing.ILicenseResultListener.Stub.asInterface(da
 this.checkLicense(_arg0, _arg1, _arg2);
 return true;
 }
-}
+default:
+{
 return super.onTransact(code, data, reply, flags);
+}
+}
 }
 private static class Proxy implements com.android.vending.licensing.ILicensingService
 {
